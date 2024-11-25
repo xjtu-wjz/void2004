4eb0baa50dc8ad32fff035e23723d2c8a8ca9f21
wjz <wjz2930608106@qq.com>
2024-11-25 16:00:26 +0800

    new

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -107,7 +107,7 @@ export default defineConfig<ThemeConfig>({
     // 请参考 https://waline.js.org/ 设置 serverURL 地址
     addonWaline({
       comment: true,
-      serverURL: 'https://waline.void2024.top/',
+      serverURL: 'https://void2024.vercel.app/',
       emoji: [/*  */],
       pageview: true,
     }),
